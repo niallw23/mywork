ae7ca429dd9d40b2c3621d9867b903fd2a95a929
Niall Wynne <nwynne93@gmail.com>
2023-02-13 21:53:13 +0000

    Lab Work - Week 03

---
+++ Week 03/testTypes.py
@@ -0,0 +1,20 @@
+# testTypes.py
+# Author = Niall Wynne
+# Creating variables for 5 different types
+# Using the type function to check
+
+i = 29
+fl = 1.5
+isa = True
+memo = 'Chiefs win the Super Bowl'
+list = ['Mahomes', 'Kelce', 'Reid']
+
+print (f'variable {i} is of type {type(i)}')
+
+print (f'variable {fl} is of type {type(fl)}')
+
+print (f'variable {isa} is of type {type(isa)}')
+
+print (f'variable {memo} is of type {type (memo)}')
+
+print (f'variable {list} is of type {type(list)}')

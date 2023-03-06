cb4d4d2d67ae92923a2cd926bc7026808e7bac07
Niall Wynne <nwynne93@gmail.com>
2023-03-06 20:19:46 +0000

    Lab Work - Week 06

--- Week 06/functions.py
+++ Week 06/functions.py
@@ -5,28 +5,53 @@
 
 ## Shows how to break your code into smaller, more manageable parts.
 
-def displaymenu ():
-    print ('What would you like to do?')
-    print ('\t(a) Add new student')
-    print ('\t(v) View students')
-    print ('\t(q) Quit')
-    choice = input('Type one letter - a/v/q :').strip ()
+def displayMenu():
+ print("What would you like to do?")
+ print("\t(a) Add new student")
+ print("\t(v) View students")
+ print("\t(q) Quit")
+ choice = input("Type one letter (a/v/q):").strip()
+ return choice
 
-    return choice
-def doadd():
-    print ('in adding')
-def doview ():
-    print ('in viewing')
+def doAdd(students):
+ currentStudent = {}
+ currentStudent["name"]=input("Enter name :")
+ currentStudent["modules"]= readModules()
+ students.append(currentStudent)
 
-choice = displaymenu ()
-while (choice != 'q'):
-    if choice == 'a':
-        doadd()
-    elif choice == 'v':               # using the first function
-        doview()
-    elif choice != 'q':
-        print ('\n\nPlease select either a, v or q')
-    choice = displaymenu ()
-    
-print (f'You chose {choice}')
+def readModules():
+ modules = []
+ moduleName = input("\tEnter the first Module name (blank to quit) :").strip()
+
+ while moduleName != "":
+    module = {}
+    module["name"]= moduleName
+    module["grade"]=int(input("\t\tEnter grade:"))
+    modules.append(module)
+    moduleName = input("\tEnter next module name (blank to quit) :").strip()
+ 
+ return modules
+
+def displayModules(modules):
+    print ("\tName \tGrade")
+    for module in modules:
+        print(f"\t{module['name']} \t{ module['grade']}")
+
+def doView(students):
+    for currentStudent in students:
+        print(currentStudent["name"])
+        displayModules(currentStudent["modules"]);
+
+
+students = []
+choice = displayMenu()
+while(choice != 'q'):
+ if choice == 'a':
+    doAdd(students)
+ elif choice == 'v':
+    doView(students)
+ elif choice !='q':
+    print("\n\nplease select either a,v or q")
+ choice=displayMenu()
+
 

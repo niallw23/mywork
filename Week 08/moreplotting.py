d68edfacce82fa97c7f36056e5938e4de02b8c8c
Niall Wynne <nwynne93@gmail.com>
2023-03-22 19:10:02 +0000

    Lab Work Week 08

--- Week 08/moreplotting.py
+++ Week 08/moreplotting.py
@@ -3,6 +3,8 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+low = 21
+high = 65
 
 min_salary = 20000
 max_salary = 80000
@@ -12,6 +14,19 @@ number_of_entries = 100
 np.random.seed(1)
 
 salaries = np.random.randint(min_salary, max_salary, number_of_entries)
+ages = np.random.randint (low, high, size = number_of_entries)
 
-plt.hist(salaries)
-plt.show()
+plt.scatter(ages, salaries, color = 'green', label = 'Salaries')
+
+xpoints = np.array(range(1,101))
+ypoints = xpoints * xpoints
+
+plt.plot(xpoints, ypoints, color = 'purple', label = 'X Squared')
+
+plt.title('Random Plot')
+plt.xlabel('Salaries')
+plt.ylabel('Age')
+plt.legend()
+# plt.show()
+
+plt.savefig('Prettier-Plot.png')

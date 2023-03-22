5603292061d386a399f3e15c339d6c8cb9eed1a8
Niall Wynne <nwynne93@gmail.com>
2023-03-22 18:04:49 +0000

    Lab Work - Week 08

--- Week 08/salaries.py
+++ Week 08/salaries.py
@@ -10,5 +10,13 @@ number_of_entries = 10
 np.random.seed(1)
 salaries = np.random.randint (min_salary, max_salary, number_of_entries) 
 
+salaries_plus = salaries + 5000
+
+salaries_mult = salaries * 1.05
 
 print (salaries)
+print (salaries_plus)
+#print (salaries_mult)
+
+new_salaries = salaries_mult.astype(int)
+print (new_salaries)

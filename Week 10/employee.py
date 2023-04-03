33bea8b6dac679f7501486dd315624f25d26448a
Niall Wynne <nwynne93@gmail.com>
2023-04-03 22:30:02 +0100

    Lab Work - Week 10

--- Week 10/employee.py
+++ Week 10/employee.py
@@ -1,5 +1,5 @@
 # Lab work week 10
-
+from timesheetentry import *
 class Employee:
     timesheets = []
 
@@ -8,9 +8,30 @@ class Employee:
         self.last = last 
     def __str__ (self):
         return self.first + ' ' + self.last
+    def logminutes (self, project, minutes):
+        now = dt.datetime.now
+        timesheetentry = Timesheetentry (project, now, minutes)
+        self.timesheets.append(timesheetentry)
+    def gettotaltime(self):
+        totalminutes = 0
+        for ts in self.timesheets:
+            totalminutes += ts.duration
+        return totalminutes
+
+
+
+
 
 if __name__ == '__main__':
     test = Employee ('Some', 'One')
     print (test)
     assert ('Some One' == str(test))
+    test.logminutes('p1', 30)
+    test.logminutes('p1', 60)
+    mins = test.gettotaltime()
+    print (mins)
+    assert( mins == 90)
+
+    print ('All good')
+
 
